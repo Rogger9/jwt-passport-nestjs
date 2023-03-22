4509a4a1534a6a50b07f1cab35ac0af8560112d4
Rogelio Gutierrez <gutierrez.rog12@gmail.com>
2023-03-22 01:06:00 -0300

    feat: create token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,12 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 
 import { CommonService } from 'src/common/common.service'
 import { User } from 'src/users/entities/user.entity'
 import { CreateUserDto, LoginDto } from './dtos'
+import { JwtPayload } from './interfaces/payload.interface'
 
 @Injectable()
 export class AuthService {
@@ -12,25 +14,35 @@ export class AuthService {
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
     private readonly commonService: CommonService,
+    private readonly jwtService: JwtService,
   ) {}
 
   async create({ password, ...data }: CreateUserDto) {
     const hash = await this.commonService.hash(password)
     const user = this.userRepository.create({ ...data, password: hash })
-    return await this.userRepository.save(user)
+    await this.userRepository.save(user)
+    return {
+      user,
+      token: this.getToken({ email: user.email }),
+    }
   }
 
   async login({ email, password }: LoginDto) {
-    try {
-      const user = await this.userRepository.findOne({
-        where: { email },
-        select: { email: true, password: true },
-      })
-      const isValidHash = await this.commonService.isValidHash(password, user.password)
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: { email: true, password: true },
+    })
+    const isValidHash = user && (await this.commonService.isValidHash(password, user.password))
 
-      return user
-    } catch (err) {
-      throw new UnauthorizedException('Invalid credentials')
+    if (!user || !isValidHash) throw new UnauthorizedException('Invalid credentials')
+
+    return {
+      user,
+      token: this.getToken({ email }),
     }
+  }
+
+  private getToken(payload: JwtPayload) {
+    return this.jwtService.sign(payload)
   }
 }
