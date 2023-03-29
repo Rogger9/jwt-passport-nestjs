ef4ca728ca7d453e0bba99add24891f51ee15c12
Rogelio Gutierrez <gutierrez.rog12@gmail.com>
2023-03-29 10:59:14 -0300

    feat: swagger inital config

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { Logger, ValidationPipe } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 import { HttpExceptionFilter } from './utils/http-exception.filter'
 
@@ -20,6 +21,14 @@ async function bootstrap() {
   app.useGlobalFilters(new HttpExceptionFilter())
 
   const configService = app.get(ConfigService)
+
+  const config = new DocumentBuilder()
+    .setTitle('Jwt api')
+    .setDescription('Jwt endpoints')
+    .setVersion('1.0')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('docs', app, document)
 
   const PORT = configService.get('config.port')
   await app.listen(PORT)
